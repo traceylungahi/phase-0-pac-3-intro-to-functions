1b44aff9df4ccf94193a6a99c6f63244014193e5
traceymmbonelungahi <t.lungahi@gmail.com>
2022-05-30 14:32:20 +0300

    commit tree 369835f76b251b9f3dcb8942977e4c9cb9c45a79

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Follow along with the examples here
+function sayHello() {
+    console.log("Hello!")
+}
+sayHello();
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!")
+}
+sayHelloToGuadalupe();
+function sayHelloToLiz() {
+    console.log("Hello, Liz!")
+}
+sayHelloToLiz(); 
+function sayHelloToSamip() {
+    console.log("Hello, Samip!")
+}
+sayHelloToSamip(); 
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`)
+}
+sayHelloTo("Guadalupe");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1);
+console.log(firstName) 
